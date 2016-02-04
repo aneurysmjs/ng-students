ae23429b2b3e68a2bf701acfab00c1a7583435e6
blackendstudios <jeroflat@gmail.com>
2016-02-03 22:14:37 -0500

    table of students

---
+++ components/appController.js
@@ -0,0 +1,19 @@
+(function () {
+   'use strict';
+
+   angular.module('app').controller('AppController', AppController);
+
+   AppController.$inject = ['appFactory'];
+
+   function AppController(appFactory) {
+      var self = this;
+
+      appFactory.retrieveData().then(function (response) {
+         self.students = response;
+      }, function (error) {
+
+      });
+
+   }
+
+}());
