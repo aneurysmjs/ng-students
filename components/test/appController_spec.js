b40dfab9cf6e0842c721a0601590b0bbc0438145
blackendstudios <jeroflat@gmail.com>
2016-02-03 22:45:49 -0500

    karma test running :)

---
+++ components/test/appController_spec.js
@@ -0,0 +1,15 @@
+describe('Controller: AppController', function () {
+
+   beforeEach(module('app'));
+
+   var ctrl;
+
+   beforeEach(inject(function ($controller) {
+      ctrl = $controller('AppController');
+   }));
+
+   it('should have the array of students', function () {
+
+   });
+
+});
