79976f08d51f7e47fba057fd3c2afcf3ed64dbc8
blackendstudios <jeroflat@gmail.com>
2016-02-14 01:53:37 -0500

    pageSize control and filter working

--- components/students/students-controller.js
+++ components/students/students-controller.js
@@ -10,19 +10,27 @@
 
       self.curPage = 0;
       self.pageSize = 5;
+      self.searchFor = '';
 
+     init();
+
+      function init() {
+         studentsFactory.retrieveData().then(function (response) {
+            console.log(' self.students');
+            console.log( self.students);
+            self.students = response;
+         }, function (error) {
+
+         });
+      }
+
+      self.deleteStudent = deleteStudent ;
       self.numberOfPages = numberOfPages;
 
-      studentsFactory.retrieveData().then(function (response) {
-         self.students = response;
-      }, function (error) {
-
-      });
-
-      self.deleteStudent = function (student) {
+      function deleteStudent(student) {
          var index = self.students.indexOf(student);
          self.students.splice(index, 1);
-      };
+      }
 
       function numberOfPages() {
          return Math.ceil(self.students.length / self.pageSize);
