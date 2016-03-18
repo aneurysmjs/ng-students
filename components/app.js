67a068da54ed3d198a5b0f795a2ab276c589940f
blackendstudios <jeroflat@gmail.com>
2016-03-18 18:58:32 -0500

    clase 4 viernes

--- components/app.js
+++ components/app.js
@@ -4,6 +4,7 @@
    // 'ui.router' es el módulo que tenemos que inyectar para poder crear nuestras rutas
    // 'students' es el módulo de estudiantes que también hay que inyectar para poder usarlo
 
+
    angular.module('app', ['ui.router', 'students']);
 
 }());
