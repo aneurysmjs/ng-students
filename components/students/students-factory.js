ecbb8ae9a86dc7fa5b6e69bbf6987563a64ee6d7
blackendstudios <jeroflat@gmail.com>
2016-03-18 21:17:20 -0500

    links

--- components/students/students-factory.js
+++ components/students/students-factory.js
@@ -1,6 +1,15 @@
 (function () {
    'use strict';
 
+   // guía mejores práctivas de john papa
+   // https://github.com/johnpapa/angular-styleguide/blob/master/a1/README.md
+
+   // patrones de diseño
+   // https://mgechev.github.io/angularjs-in-patterns/
+
+   // tero parvianen
+   // http://teropa.info/
+
    angular.module('students').factory('studentsFactory', studentsFactory);
 
 
