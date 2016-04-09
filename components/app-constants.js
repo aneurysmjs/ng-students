3853298d00317c874a6abb73f2e42dd1c9171814
blackendstudios <jeroflat@gmail.com>
2016-04-09 12:50:45 -0500

    refactor(students-controller): renombrar API_URL con la dirección IP de rails-students

--- components/app-constants.js
+++ components/app-constants.js
@@ -1,6 +1,9 @@
 (function () {
    'use strict';
 
-   angular.module('app').constant('API_URL', 'http://181.55.68.207:3000/api/v1');
+   //angular.module('app').constant('API_URL', 'http://181.55.68.207:3000/api/v1');
+
+   // UAO
+   angular.module('app').constant('API_URL', 'http://172.16.129.76:3000/api/v1');
 
 }());
