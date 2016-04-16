1790e62ed7e604d62fee91772270786858803618
blackendstudios <jeroflat@gmail.com>
2016-04-15 20:01:02 -0500

    feat(students): explicación de compile, pre-link y post-link

--- components/students/components/studentsTable/studentsTable-directive.js
+++ components/students/components/studentsTable/studentsTable-directive.js
@@ -20,8 +20,28 @@
             studentsList: '='
          },
          restrict: 'E',
-         templateUrl: componentRoute + 'studentsTable/studentsTable.html'
+         templateUrl: componentRoute + 'studentsTable/studentsTable.html',
+         compile: compile
+
       };
+
+      function compile(tElement, tAttrs) {
+         console.log('estoy en compile');
+         return {
+            pre: function (scope, element, attrs) {
+               console.log('estoy en pre-link');
+            },
+            post: function (scope, element, attrs) {
+               console.log('estoy en post-link');
+            }
+         }
+      }
+
+      /*function link(scope) {
+         console.log('scope');
+         console.log(scope);
+
+      }*/
 
 
    }
