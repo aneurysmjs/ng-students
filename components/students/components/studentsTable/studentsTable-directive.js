3e0c3058efaa1ec5dc2bb84f776feabad4a7db3a
blackendstudios <jeroflat@gmail.com>
2016-04-15 20:11:15 -0500

    feat(students): uso del método component para crear pedazos de interfaz

--- components/students/components/studentsTable/studentsTable-directive.js
+++ components/students/components/studentsTable/studentsTable-directive.js
@@ -1,49 +1,21 @@
 (function () {
    'use strict';
 
-   angular.module('students').directive('studentsTable', studentsTable);
+   var studentsTable = {
+      controller: [function StudentsTableController() {
+         var self  = this;
+         console.log('self');
+         console.log(self);
+      }],
+      bindings: {
+         studentsList: '='
+      },
+      templateUrl: 'components/students/components/studentsTable/studentsTable.html'
 
-   studentsTable.$inject = ['componentRoute'];
+   };
 
-   function studentsTable(componentRoute) {
-
-      return {
-         scope: {},
-         controller: [function StudentsTableController() {
-            var self  = this;
-
-            console.log('self');
-            console.log(self);
-         }],
-         controllerAs: 'ctrl',
-         bindToController: {
-            studentsList: '='
-         },
-         restrict: 'E',
-         templateUrl: componentRoute + 'studentsTable/studentsTable.html',
-         compile: compile
-
-      };
-
-      function compile(tElement, tAttrs) {
-         console.log('estoy en compile');
-         return {
-            pre: function (scope, element, attrs) {
-               console.log('estoy en pre-link');
-            },
-            post: function (scope, element, attrs) {
-               console.log('estoy en post-link');
-            }
-         }
-      }
-
-      /*function link(scope) {
-         console.log('scope');
-         console.log(scope);
-
-      }*/
+   angular.module('students').component('studentsTable', studentsTable);
 
 
-   }
 
 }());
